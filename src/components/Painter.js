23710f0814c89f2df844b3f08cc1d67bb50ce79f
lichen <chinalichen@126.com>
2018-07-01 00:10:55 +0800

    support multiple touch.

--- src/components/Painter.js
+++ src/components/Painter.js
@@ -1,109 +1,6 @@
 import React, { Component } from 'react';
 import './Painter.css';
 
-// import TweenLite, { Linear, TimelineMax } from 'gsap/TweenMax';
-// import Draggable from 'gsap/Draggable';
-
-// TweenLite.defaultEase = Linear.easeNone;
-
-// const perf = window.performance;
-// const now = perf ? perf.now.bind(perf) : Date.now;
-
-// const pencilPoint = document.querySelector("#pencil-point");
-// const pencilPath = document.querySelector("#pencil-path");
-// const smoothPath = document.querySelector("#smooth-path");
-
-// let points = [];
-// let strokes = [];
-// let prev = null;
-// let start = 0;
-
-// const tl = new TimelineMax();
-
-// let pencil = new Draggable(pencilPoint, {
-// 	bounds: "#svg-drawing",
-// 	trigger: "#svg-drawing",
-// 	cursor: "crosshair",
-// 	onDrag,
-// 	onDragEnd,
-// 	onPress
-// });
-
-// function onPress() {
-
-// 	tl.clear();
-
-// 	start = now();
-
-// 	pencilPath.setAttribute("points", "");
-// 	smoothPath.setAttribute("d", "");
-
-// 	let x = this.pointerX;
-// 	let y = this.pointerY;
-
-// 	let stroke = {
-// 		elapsed: 0,
-// 		time: start,
-// 		dist: 0,
-// 		x: x,
-// 		y: y
-// 	};
-
-// 	points = [x, y];
-// 	strokes = [stroke];
-// 	prev = stroke;
-
-// 	TweenLite.set(pencilPoint, { x, y });
-// 	this.update();
-// }
-
-// function onDrag() {
-
-// 	let time = now();
-// 	let elapsed = time - prev.time;
-
-// 	let dist = getDistance(prev, this);
-
-// 	let x = this.endX;
-// 	let y = this.endY;
-
-// 	let stroke = {
-// 		elapsed: elapsed,
-// 		time: time,
-// 		dist: prev.dist + dist,
-// 		x: x,
-// 		y: y
-// 	};
-
-// 	prev = stroke;
-// 	strokes.push(stroke);
-// 	points.push(x, y);
-// 	pencilPath.setAttribute("points", points);
-// }
-
-// function onDragEnd() {
-
-// 	let totalTime = (now() - start) / 1000;
-// 	let totalDist = strokes[strokes.length - 1].dist;
-
-// 	smoothPath.setAttribute("d", solve(points));
-// 	pencilPath.setAttribute("points", "");
-
-// 	strokes.reduce((tl, stroke) => {
-
-// 		let time = stroke.elapsed / 1000;
-// 		let draw = (stroke.dist / totalDist * 100) + "%";
-
-// 		return tl.to(smoothPath, time, { drawSVG: draw });
-// 	}, tl);
-// }
-
-// function getDistance(p1, p2) {
-// 	let dx = p2.x - p1.x;
-// 	let dy = p2.y - p1.y;
-// 	return Math.sqrt(dx * dx + dy * dy);
-// }
-
 function solve(data, k = 1) {
   const size = data.length;
   const last = size - 4;
@@ -135,47 +32,110 @@ function solve(data, k = 1) {
   return path;
 }
 
+function hasXY(evt) {
+  if (evt.clientX == null) {
+    return false;
+  }
+  if (evt.clientY == null) {
+    return false;
+  }
+  return true;
+}
+
 export default class Painter extends Component {
   constructor(props) {
     super(props);
     this.handleDrawStart = this.handleDrawStart.bind(this);
     this.handleDrawMove = this.handleDrawMove.bind(this);
     this.handleDrawEnd = this.handleDrawEnd.bind(this);
-    this.state = { paths: [] };
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
+    this.state = { paths: [], currentPaths: [] };
     this.points = null;
     this.count = 0;
   }
+  hasPoints() {
+    if (!this.pointsList) {
+      return false;
+    }
+    return this.pointsList.some(points => points && points.length > 0);
+  }
+  updatePoints(idx, x, y) {
+    if (!this.pointsList) {
+      this.pointsList = [];
+    }
+    this.pointsList[idx] = (this.pointsList[idx] || []).concat([x, y]);
+  }
   updateCurrentPath() {
-    if (this.points) {
-      this.setState({ currentPath: solve(this.points) });
+    if (this.hasPoints()) {
+      this.setState({ currentPaths: this.pointsList.map(ps => solve(ps)) });
     }
   }
   handleDrawStart(evt) {
-    this.points = [evt.clientX, evt.clientY];
+    this.updatePoints(0, evt.clientX, evt.clientY);
     this.updateCurrentPath();
   }
   handleDrawMove(evt) {
-    if (!this.points) {
+    if (!this.hasPoints()) {
       return;
     }
-    this.points = this.points.concat([evt.clientX, evt.clientY]);
+    this.updatePoints(0, evt.clientX, evt.clientY);
     this.updateCurrentPath();
   }
   handleDrawEnd(evt) {
-    if (!this.points) {
+    if (!this.hasPoints()) {
       return;
     }
-    this.points = this.points.concat([evt.clientX, evt.clientY]);
-    const d = solve(this.points, 1);
-    this.points = null;
+    this.updatePoints(0, evt.clientX, evt.clientY);
+    const d = solve(this.pointsList[0], 1);
+    this.pointsList = null;
     const paths = this.state.paths.concat({ id: (this.count += 1), d });
-    this.setState({ paths, currentPath: null });
+    this.setState({ paths, currentPaths: [] });
+  }
+  handleTouchStart(evt) {
+    Array.from(evt.touches).forEach((te, idx) => {
+      if (hasXY(te)) {
+        this.updatePoints(idx, te.clientX, te.clientY);
+      }
+    });
+  }
+  handleTouchMove(evt) {
+    if (!this.hasPoints()) {
+      return;
+    }
+    Array.from(evt.touches).forEach((te, idx) => {
+      if (hasXY(te)) {
+        this.updatePoints(idx, te.clientX, te.clientY);
+      }
+    });
+    this.updateCurrentPath();
+  }
+  handleTouchEnd(evt) {
+    if (!this.hasPoints()) {
+      return;
+    }
+    Array.from(evt.touches).forEach((te, idx) => {
+      if (hasXY(te)) {
+        this.updatePoints(idx, te.clientX, te.clientY);
+      }
+    });
+    const ds = this.pointsList
+      .filter(ps => ps && ps.length > 0)
+      .map(ps => solve(ps, 1))
+      .map(d => ({ id: (this.count += 1), d }));
+    this.pointsList = null;
+    const paths = this.state.paths.concat(ds);
+    this.setState({ paths, currentPaths: [] });
   }
   render() {
     return (
       <div
         className="painter-container"
         role="presentation"
+        onTouchStart={this.handleTouchStart}
+        onTouchMove={this.handleTouchMove}
+        onTouchEnd={this.handleTouchEnd}
         onMouseDown={this.handleDrawStart}
         onMouseMove={this.handleDrawMove}
         onMouseUp={this.handleDrawEnd}
@@ -186,9 +146,7 @@ export default class Painter extends Component {
           ))}
         </svg>
         <svg className="svgDrawing">
-          {this.state.currentPath && (
-            <path className="paths" d={this.state.currentPath} />
-          )}
+          {this.state.currentPaths.map((d, i) => <path className="paths" key={i} d={d} />)}
         </svg>
       </div>
     );
