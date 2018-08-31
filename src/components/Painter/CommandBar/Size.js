a86bd525c876c3ce89a029b733e4e3525ecab784
chinalichen <chinalichen@126.com>
2018-08-31 23:11:12 +0800

    add custom size input.

--- src/components/Painter/CommandBar/Size.js
+++ src/components/Painter/CommandBar/Size.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from 'antd/lib/menu';
 import Dropdown from 'antd/lib/dropdown';
 import PropTypes from 'prop-types';
+import { InputNumber } from 'antd';
 
 export default class Size extends Component {
   static propTypes = {
@@ -11,6 +12,9 @@ export default class Size extends Component {
   handleSizeChange(size) {
     this.props.onSizeChange(size);
   }
+  getSize() {
+    return this.props.size || 2;
+  }
   getSizeMenu() {
     return (
       <Menu>
@@ -18,6 +22,8 @@ export default class Size extends Component {
         <Menu.Item onClick={this.handleSizeChange.bind(this, 4)}>4</Menu.Item>
         <Menu.Item onClick={this.handleSizeChange.bind(this, 6)}>6</Menu.Item>
         <Menu.Item onClick={this.handleSizeChange.bind(this, 8)}>8</Menu.Item>
+        <Menu.Item onClick={this.handleSizeChange.bind(this, 10)}>10</Menu.Item>
+        <InputNumber min={1} max={100} defaultValue={this.getSize()} onChange={this.handleSizeChange.bind(this)} />
       </Menu>
     );
   }
@@ -27,7 +33,7 @@ export default class Size extends Component {
         Size
         <div>
           <Dropdown overlay={this.getSizeMenu()}>
-            <a>{this.props.size || 2}</a>
+            <a>{this.getSize()}</a>
           </Dropdown>
         </div>
       </div>
