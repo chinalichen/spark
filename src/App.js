23e1da663c164cf792fe6abf9f939288e67410e8
chinalichen <chinalichen@126.com>
2018-08-31 22:46:54 +0800

    split code by router.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import Route from 'react-router-dom/Route';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
-import Docs from './components/Docs/Docs';
-import About from './components/About/About';
-import Designer from './components/Designer/Designer';
+// import Docs from './components/Docs/Docs';
+// import About from './components/About/About';
+// import Designer from './components/Designer/Designer';
 
 import 'antd/dist/antd.css';
 import './App.css';
+import asyncComponent from './components/AsyncComponent';
+
+const AsyncDocs = asyncComponent(() => import('./components/Docs/Docs'));
+const AsyncDesigner = asyncComponent(() => import('./components/Designer/Designer'));
+const AsyncAbout = asyncComponent(() => import('./components/About/About'));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route exact path="/" component={Docs} />
-          <Route path="/designer/:docID" component={Designer} />
-          <Route path="/about" component={About} />
+          <Route exact path="/" component={AsyncDocs} />
+          <Route path="/designer/:docID" component={AsyncDesigner} />
+          <Route path="/about" component={AsyncAbout} />
         </div>
       </BrowserRouter>
     );
