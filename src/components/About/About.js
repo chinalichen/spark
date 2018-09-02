b26d9a0554ac4a9e304710764ffc5c35706f5f12
chinalichen <chinalichen@126.com>
2018-09-02 23:02:23 +0800

    update About page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+import Layout from 'antd/lib/layout';
+import { FormattedRelative } from 'react-intl';
+
+import 'antd/es/layout/style/index.css';
+import './About.css';
 
 export default class About extends Component {
   render() {
     return (
-      <div>about</div>
+      <div className="aboutContainer">
+        <Layout className="layout">
+          <Layout.Header className="header">
+            <div className="logo">
+              <Link to="/">Spark</Link>
+            </div>
+          </Layout.Header>
+          <Layout.Content className="content">
+            <p>可以一起创作哦！</p>
+          </Layout.Content>
+          <Layout.Footer className="footer">
+            ©2018 Created by <Link to="/about">Lichen</Link>
+          </Layout.Footer>
+        </Layout>
+      </div >
     );
   }
 }
