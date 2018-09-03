c9c365a568827ed883f7497a027c307646e62892
lichen <chinalichen@126.com>
2018-09-03 20:32:43 +0800

    split code.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 import Route from 'react-router-dom/Route';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
-import Docs from './components/Docs/Docs';
-import About from './components/About/About';
-import Designer from './components/Designer/Designer';
-
-import stylesheet from 'antd/lib/style/index.css';
-import style_badge from 'antd/lib/badge/style/index.css';
+// import Docs from './components/Docs/Docs';
+// import About from './components/About/About';
+// import Designer from './components/Designer/Designer';
 
 import './App.css';
-// import asyncComponent from './components/AsyncComponent';
+import asyncComponent from './components/AsyncComponent';
 
-// const Docs = asyncComponent(() => import('./components/Docs/Docs'));
-// const Designer = asyncComponent(() => import('./components/Designer/Designer'));
-// const About = asyncComponent(() => import('./components/About/About'));
+const Docs = asyncComponent(() => import('./components/Docs/Docs'));
+const Designer = asyncComponent(() => import('./components/Designer/Designer'));
+const About = asyncComponent(() => import('./components/About/About'));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-          <style dangerouslySetInnerHTML={{ __html: style_badge }} />
           <Route exact path="/" component={Docs} />
           <Route path="/designer/:docID" component={Designer} />
           <Route path="/about" component={About} />
--- src/components/Painter/NavigationBar.js
+++ src/components/Painter/NavigationBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Icon from 'antd/lib/icon';
 import Link from 'react-router-dom/Link';
 import './NavigationBar.css';
-import 'antd/es/icon/style';
+import 'antd/es/icon/style/css';
 
 export default class NavigationBar extends Component {
   render() {
