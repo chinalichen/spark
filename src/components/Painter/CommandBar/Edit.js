af183f71409d3cbee99cb30d3333bf4be0169e32
lichen <chinalichen@126.com>
2018-09-07 14:11:26 +0800

    add margin between undo and redo button.

--- src/components/Painter/CommandBar/Edit.js
+++ src/components/Painter/CommandBar/Edit.js
@@ -10,7 +10,7 @@ export default class Edit extends Component {
   };
   render() {
     return (
-      <div className="section">
+      <div className="section edit">
         Edit
         <div className="horizontalIcons">
           <a title="undo" onClick={this.props.undo}>
